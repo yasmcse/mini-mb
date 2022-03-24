32c9af650ac115c115db820855972936abc24b48
Jaeren Coathup <jaeren@moneyboxapp.com>
2022-03-24 16:32:44 +0000

    Add extra model

--- app/src/main/java/com/moneybox/minimb/data/models/products/AccountResponse.kt
+++ app/src/main/java/com/moneybox/minimb/data/models/products/AccountResponse.kt
@@ -5,6 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class ProductResponse(
     @SerializedName("Id")
     val id: Int,
+    @SerializedName("Product")
+    val product: ProductDetailsResponse,
     @SerializedName("Moneybox")
     val moneybox: Float,
     @SerializedName("PlanValue")
